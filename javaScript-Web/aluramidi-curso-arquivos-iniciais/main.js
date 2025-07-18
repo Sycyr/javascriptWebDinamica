524005c82cdad780045654f22ecd149b2f0f3298
Fabricio Cima <132305249+Sycyr@users.noreply.github.com>
2025-07-18 15:46:54 -0400

    Projeto inicial
    Projeto inacabado para dar continuidade em outro dispositivo.

---
+++ javaScript-Web/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,15 @@
+function tocaSomPom() {
+    const play = document.querySelector('[data-audio-pom]').play();
+}
+
+const pom = document.querySelector('[data-tecla="pom"');
+
+pom.addEventListener('click',tocaSomPom);
+
+const listaDeTeclas = document.querySelectorAll('[data-tecla]');
+
+
+
+
+
+
